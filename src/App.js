bdd3643d81734b0be6a72f5d047a6a3b8b85a645
Varshita4466 <94522188+Varshita4466@users.noreply.github.com>
2023-10-22 17:11:54 +0530

    task5

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AgeCalculator from './components/Agecalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
+
